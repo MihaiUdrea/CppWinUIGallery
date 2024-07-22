b399a151a02d2ea0e1aaf4d6f829b765d201e59f
SabinaComandasu <comandasu.sabina.x9g@student.ucv.ro>
2024-07-22 10:27:53 +0300

    Added Show C++ Button and TextBox that displays the source code for the ToggleSwitch.

--- src/ToggleSwitchPage.xaml.cpp
+++ src/ToggleSwitchPage.xaml.cpp
@@ -62,14 +62,43 @@ namespace winrt::CppWinUIGallery::implementation
         if (textBox.Visibility() == Visibility::Collapsed)
         {
             // Hide C++ code if visible
-            if (cppTextBox.Visibility() == Visibility::Visible)
+           /* if (cppTextBox.Visibility() == Visibility::Visible)
             {
                 cppTextBox.Visibility(Visibility::Collapsed);
                 cppButton.Content(box_value(L"Show C++"));
-            }
+            }*/
 
             // Show XAML source code
-            hstring xamlSourceCode = L"<!-- XAML code here -->";
+            hstring xamlSourceCode = LR"(
+<TextBlock Text="ToggleSwitch" Style="{StaticResource TitleLargeTextBlockStyle}" Margin="50,40"/>
+
+<!-- SUBSCRIPTION BLOCK -->
+<Border x:Name="SubscriptionBorder" Background="Green" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="40,116,140,0" CornerRadius="20" Width="210" Canvas.ZIndex="0">
+    <TextBlock x:Name="SubscriptionText" Text="Subscription" Foreground="White" HorizontalAlignment="Center" VerticalAlignment="Center" Margin="10" />
+</Border>
+
+<!-- PERPETUAL BLOCK -->
+<Border x:Name="PerpetualBorder" HorizontalAlignment="Left" Background="Blue" VerticalAlignment="Top" Margin="190,116,0,0" CornerRadius="20" Padding="10" Width="210" Canvas.ZIndex="0">
+    <TextBlock x:Name="PerpetualText" Text="Perpetual" Foreground="White" VerticalAlignment="Center" HorizontalAlignment="Center" Margin="0,0,0,0" />
+</Border>
+
+<!-- TOGGLE SWITCH -->
+<ToggleSwitch x:Name="SubscriptionToggleSwitch" Toggled="SubscriptionToggleSwitch_Toggled" VerticalAlignment="Top" Width="55" Height="35" Margin="199,113,0,10" Canvas.ZIndex="1">
+    <!-- Style to hide on/off text -->
+    <ToggleSwitch.Resources>
+        <Style TargetType="ToggleSwitch">
+            <Setter Property="Header" Value=""/>
+            <Setter Property="HeaderTemplate">
+                <Setter.Value>
+                    <DataTemplate>
+                        <!-- Empty template to hide the header text -->
+                    </DataTemplate>
+                </Setter.Value>
+            </Setter>
+        </Style>
+    </ToggleSwitch.Resources>
+</ToggleSwitch>
+)";
             textBox.Text(xamlSourceCode);
             textBox.Visibility(Visibility::Visible);
             button.Content(box_value(L"Hide XAML"));
@@ -93,14 +122,42 @@ namespace winrt::CppWinUIGallery::implementation
         if (textBox.Visibility() == Visibility::Collapsed)
         {
             // Hide XAML code if visible
-            if (xamlTextBox.Visibility() == Visibility::Visible)
+           /* if (xamlTextBox.Visibility() == Visibility::Visible)
             {
                 xamlTextBox.Visibility(Visibility::Collapsed);
                 xamlButton.Content(box_value(L"Show XAML"));
-            }
+            }*/
 
             // Show C++ source code
-            hstring cppSourceCode = L"// C++ code here";
+            hstring cppSourceCode = LR"(
+void ToggleSwitchPage::SubscriptionToggleSwitch_Toggled(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        auto toggleSwitch = SubscriptionToggleSwitch();
+        auto subscriptionBorder = SubscriptionText().Parent().as<Border>();
+        auto perpetualBorder = PerpetualText().Parent().as<Border>();
+
+        SolidColorBrush greenBrush{ Colors::Green() };
+        SolidColorBrush blueBrush{ Colors::Blue() };
+        SolidColorBrush whiteBrush{ Colors::White() };
+        auto customColor = Windows::UI::ColorHelper::FromArgb(58, 56, 245, 212);
+        SolidColorBrush customBrush{ customColor };
+
+        if (toggleSwitch.IsOn())
+        {
+            subscriptionBorder.Background(customBrush);
+            perpetualBorder.Background(blueBrush);
+            perpetualBorder.Child().as<TextBlock>().Foreground(whiteBrush);
+        }
+        else
+        {
+            subscriptionBorder.Background(greenBrush);
+            subscriptionBorder.Child().as<TextBlock>().Foreground(whiteBrush);
+            perpetualBorder.Background(customBrush);
+        }
+    }
+)";
             textBox.Text(cppSourceCode);
             textBox.Visibility(Visibility::Visible);
             button.Content(box_value(L"Hide C++"));
