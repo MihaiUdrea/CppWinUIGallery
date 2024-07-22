1430f0c18a15336a1ba12c672d77952378cb1059
SabinaComandasu <comandasu.sabina.x9g@student.ucv.ro>
2024-07-22 14:46:17 +0300

    ﻿Created FloatingInfoBar design.

---
+++ src/FloatingElementPage.xaml.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "FloatingElementPage.xaml.h"
+#if __has_include("FloatingElementPage.g.cpp")
+#include "FloatingElementPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::CppWinUIGallery::implementation
+{
+    int32_t FloatingElementPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void FloatingElementPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+
+}
