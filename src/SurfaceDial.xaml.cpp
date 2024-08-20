176673deaedecf3dc9e9520c6db435cbb87c93cb
SabinaComandasu <comandasu.sabina.x9g@student.ucv.ro>
2024-08-20 17:06:47 +0300

    Fixed Surface Dial mechanics.

--- src/SurfaceDial.xaml.cpp
+++ src/SurfaceDial.xaml.cpp
@@ -23,27 +23,104 @@ namespace winrt::CppWinUIGallery::implementation
     {
         throw hresult_not_implemented();
     }
-    void winrt::CppWinUIGallery::implementation::SurfaceDial::hoverSection_PointerEntered(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& /*e*/)
+    bool m_timerActive{ false };
+
+    void SurfaceDial::hoverSection_PointerEntered(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& /*e*/)
     {
-        // Cast sender to Path
         auto hoverSection = sender.as<winrt::Microsoft::UI::Xaml::Shapes::Path>();
 
-        // Change the Fill color
+        auto tag = hoverSection.Tag().as<winrt::hstring>();
+        auto menuSection = FindName(tag).as<winrt::Microsoft::UI::Xaml::Shapes::Path>();
+
+        if (!menuSection)
+        {
+            return;
+        }
+
+        // Stop any existing timer if active
+        if (m_timerActive)
+        {
+            m_timer.Stop();
+            m_timerActive = false;
+
+            // Reset the previous menuSection color if it exists
+            if (m_previousMenuSection)
+            {
+                m_previousMenuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x00, 0x00)));
+            }
+        }
+
+        // Update current hoverSection and menuSection colors immediately
         hoverSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x19, 0x19, 0x19)));
+        menuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x19, 0x19, 0x19)));
+
+        // Set the new menuSection to be managed by the timer
+        m_previousMenuSection = menuSection;
+
+        if (!m_timer)
+        {
+            m_timer = winrt::Microsoft::UI::Xaml::DispatcherTimer{};
+            m_timer.Interval(winrt::Windows::Foundation::TimeSpan{ 1000000LL }); // 1 second (10,000,000 ticks)
+
+            m_timer.Tick([this](auto const&, auto const&)
+                {
+                    if (m_timerActive && m_previousMenuSection)
+                    {
+                        m_previousMenuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x78, 0xD7)));
+                    }
+                    m_timer.Stop();
+                    m_timerActive = false;
+                });
+        }
+
+        m_timerActive = true;
+        m_timer.Start();
     }
 
-    void winrt::CppWinUIGallery::implementation::SurfaceDial::hoverSection_PointerExited(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& /*e*/)
+    void SurfaceDial::hoverSection_PointerExited(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& /*e*/)
     {
-        // Cast sender to Path
         auto hoverSection = sender.as<winrt::Microsoft::UI::Xaml::Shapes::Path>();
 
-        // Change the Fill color
+        auto tag = hoverSection.Tag().as<winrt::hstring>();
+        auto menuSection = FindName(tag).as<winrt::Microsoft::UI::Xaml::Shapes::Path>();
+
+        if (!menuSection)
+        {
+            return;
+        }
+
+        if (m_timerActive)
+        {
+            m_timer.Stop();
+            m_timerActive = false;
+
+            // Reset the previous menuSection color to black if it exists
+            if (m_previousMenuSection)
+            {
+                m_previousMenuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x00, 0x00)));
+            }
+        }
+
+        // Reset the hoverSection color to black
         hoverSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x00, 0x00)));
+        menuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x00, 0x00)));
+
+        // If the pointer exited and a menuSection was active, ensure it is reset to black
+        if (m_previousMenuSection && m_previousMenuSection != menuSection)
+        {
+            m_previousMenuSection.Fill(winrt::Microsoft::UI::Xaml::Media::SolidColorBrush(winrt::Windows::UI::ColorHelper::FromArgb(0xFF, 0x00, 0x00, 0x00)));
+        }
+
+        // Clear the previous menuSection reference as it no longer applies
+        m_previousMenuSection = nullptr;
     }
 
     
 }
-
 
 
 
--- src/SurfaceDial.xaml.h
+++ src/SurfaceDial.xaml.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include "SurfaceDial.g.h"
+#include "winrt/Windows.System.Threading.h"
+#include "winrt/Windows.UI.Xaml.Controls.h"
+#include "winrt/Windows.UI.Xaml.Input.h"
+#include "winrt/Windows.UI.Xaml.Shapes.h"
+#include "winrt/Windows.UI.Xaml.Media.h"
+#include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.UI.Xaml.h"
 
 namespace winrt::CppWinUIGallery::implementation
 {
@@ -17,6 +24,9 @@ namespace winrt::CppWinUIGallery::implementation
 
         void hoverSection_PointerEntered(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
         void hoverSection_PointerExited(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        winrt::Microsoft::UI::Xaml::DispatcherTimer m_timer{ nullptr };
+        winrt::Microsoft::UI::Xaml::Shapes::Path m_previousMenuSection{ nullptr };
+
     };
 }
 
