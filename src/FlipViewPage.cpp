0f8cee328392e174ffa190587b04d915ad00a56a
Liviu Prioteasa <liviu.prioteasa.274@gmail.com>
2024-09-09 16:12:46 +0300

    Added FlipItem local element binding for FlipView custom content population using DataTemplate

---
+++ src/FlipViewPage.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+
+#include "FlipViewPage.h"
+#if __has_include("FlipViewPage.g.cpp")
+#include "FlipViewPage.g.cpp"
+#endif
+
+#if __has_include("FlipItem.g.cpp")
+#include "FlipItem.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+namespace winrt::CppWinUIGallery::implementation
+{
+int32_t FlipViewPage::MyProperty()
+{
+    throw hresult_not_implemented();
+}
+
+void FlipViewPage::MyProperty(int32_t /* value */)
+{
+    throw hresult_not_implemented();
+}
+
+FlipItem::FlipItem(hstring image, hstring name)
+{
+    this->m_image = image;
+    this->m_name = name;
+}
+
+void FlipViewPage::FlipView_Loaded(winrt::Windows::Foundation::IInspectable const &sender,
+                                   winrt::Microsoft::UI::Xaml::RoutedEventArgs const &e)
+{
+    auto items = single_threaded_observable_vector<CppWinUIGallery::FlipItem>();
+    items.Append(make<FlipItem>(L"ms-appx:///Assets/ai_logo.png", L"Cliff"));
+
+    /*auto item = make<FlipItem>();
+    items.Append(item);*/
+    StoreFlipView().ItemsSource(items);
+}
+
+} // namespace winrt::CppWinUIGallery::implementation
---
+++ src/FlipViewPage.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include "FlipItem.g.h"
+#include "FlipViewPage.g.h"
+
+namespace winrt::CppWinUIGallery::implementation
+{
+struct FlipViewPage : FlipViewPageT<FlipViewPage>
+{
+
+    FlipViewPage()
+    {
+        // Xaml objects should not call InitializeComponent during construction.
+        // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+    }
+
+    int32_t MyProperty();
+    void MyProperty(int32_t value);
+    void FlipView_Loaded(winrt::Windows::Foundation::IInspectable const &sender,
+                         winrt::Microsoft::UI::Xaml::RoutedEventArgs const &e);
+};
+
+struct FlipItem : FlipItemT<FlipItem>
+{
+    FlipItem() = default;
+    FlipItem(hstring image, hstring name);
+
+    hstring Image()
+    {
+        return m_image;
+    }
+    void Image(hstring const &value)
+    {
+        m_image = value;
+    }
+
+    hstring Name()
+    {
+        return m_name;
+    }
+    void Name(hstring const &value)
+    {
+        m_name = value;
+    }
+
+  private:
+    hstring m_image{L""};
+    hstring m_name{L""};
+};
+
+} // namespace winrt::CppWinUIGallery::implementation
+
+namespace winrt::CppWinUIGallery::factory_implementation
+{
+struct FlipViewPage : FlipViewPageT<FlipViewPage, implementation::FlipViewPage>
+{
+};
+
+struct FlipItem : FlipItemT<FlipItem, implementation::FlipItem>
+{
+};
+
+} // namespace winrt::CppWinUIGallery::factory_implementation
